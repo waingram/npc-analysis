4b7e1a6de838416f31176e3711090fc02d6a483c
Bill Ingram <waingram@gmail.com>
2020-08-02 17:58:48 -0400

    modify jar filename

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ mv /tmp/pom.xml .
 mvn clean compile assembly:single
 
 # This is the master script for the capsule. When you click "Reproducible Run", the code in this file will execute.
-java -jar target/npc-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/neural-parscit-evaluation-1.0-SNAPSHOT-jar-with-dependencies.jar
